db619db8c709a2338d5ea07bab232727328e7099
LucasKarjel <113012215+LucasKarjel@users.noreply.github.com>
2022-09-20 12:08:29 +0200

    Assignment 05

---
+++ Assignment 05/Movement/Assets/Scripts/Movement.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : ProcessingLite.GP21
+{
+    float posX = 0;
+    float diameter = 2;
+    float speed = 5;
+    float posY;
+    float v = 1;
+    int a = 1;
+    // Start is called before the first frame update
+    void Start()
+    {
+        posX = Width / 2;
+        posY = Height / 2;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Background(50, 166, 240);
+        posX += Input.GetAxisRaw("Horizontal") * speed * v * Time.deltaTime;
+        posY += Input.GetAxisRaw("Vertical") * speed * v * Time.deltaTime;
+        
+
+        //posX = posX - v * Time.deltaTime;
+        //posY = posY - v * Time.deltaTime;
+
+        if (v < 10)
+            v += a * Time.deltaTime;
+
+        Square(posX - 5, posY, 2);
+        Circle(posX, posY, diameter);
+    }
+}
