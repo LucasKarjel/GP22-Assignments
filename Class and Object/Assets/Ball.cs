914118f9ed5281fc290ead9bc838cbd6937bbb1a
LucasKarjel <lucas.karjel@gmail.com>
2022-09-30 14:57:52 +0200

    Added color and size randomizer

---
+++ Class and Object/Assets/Ball.cs
@@ -0,0 +1,53 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//We still need to inherence from ProcessingLite, so we get access to all functions
+public class Ball : ProcessingLite.GP21
+{
+    //Our class variables
+    Vector2 position; //Ball position
+    Vector2 velocity; //Ball direction
+    int color1;
+    int color2;
+    int color3;
+    public float size;
+    public float radius;
+
+    //Ball Constructor, called when we type new Ball(x, y);
+    public Ball(float x, float y)
+    {
+        //Set our position when we create the code.
+        position = new Vector2(x, y);
+        //Set random color
+        color1 = Random.Range(0, 255);
+        color2 = Random.Range(0, 255);
+        color3 = Random.Range(0, 255);
+        NoStroke();
+        //Set random size
+        size = Random.Range(0.5f, 1f);
+        radius = size / 2;
+        //Create the velocity vector and give it a random direction.
+        velocity = new Vector2();
+        velocity.x = Random.Range(0, 11) - 5;
+        velocity.y = Random.Range(0, 11) - 5;
+    }
+    //Draw our ball
+    public void Draw()
+    {
+        Fill(color1, color2, color3);
+        Circle(position.x, position.y, size);
+        Debug.Log(size);
+    }
+
+    //Update our ball
+    public void UpdatePos()
+    {
+        if (position.y + radius > Height && velocity.y < 0 || position.y - radius < 0 && velocity.y > 0)
+            velocity.y *= -1;
+        else if (position.x + radius > Width && velocity.x < 0 || position.x - radius < 0 && velocity.x > 0)
+            velocity.x *= -1;
+
+        position += velocity * Time.deltaTime;
+    }
+}
---
+++ Class and Object/Assets/Player.cs
@@ -0,0 +1,71 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : ProcessingLite.GP21
+{
+    Ball[] balls;
+    int numberOfBalls = 10;
+    public float diameter = 2;
+    public float speed = 5;
+    public float velocity = 0;
+    public float accelerate = 10;
+    public float decelerate = 20;
+    public float maxSpeed = 10;
+    Vector2 circlePos;
+    Vector2 lastInput;
+    // Start is called before the first frame update
+    void Start()
+    {
+        circlePos = new Vector2(Width / 2, Height / 2);
+
+        balls = new Ball[numberOfBalls];
+
+        //A loop that can be used for creating multiple balls.
+        for (int i = 0; i < balls.Length; i++)
+        {
+            balls[i] = new Ball(5, 5);
+        }
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        Background(0);
+        Vector2 RawInput = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical")).normalized;
+        Vector2 SmoothInput = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical")).normalized;
+
+        //Set arguments for velocity and boundaries for max speed
+        if (RawInput.magnitude != 0)
+        {
+            lastInput = SmoothInput;
+        }
+        if (RawInput.magnitude != 0)
+        {
+            velocity += accelerate * Time.deltaTime;
+        }
+        else if (RawInput.magnitude == 0)
+        {
+            velocity -= decelerate * Time.deltaTime;
+        }
+
+        if (velocity < 0)
+            velocity = 0;
+
+        if (velocity > maxSpeed)
+            velocity = maxSpeed;
+
+        circlePos += lastInput * velocity * Time.deltaTime;
+
+        //Update player position
+        Stroke(255, 0, 0);
+        Fill(0);
+        Circle(circlePos.x, circlePos.y, diameter);
+        NoStroke();
+        //Tell each ball to update it's position
+        for (int i = 0; i < balls.Length; i++)
+         {
+             balls[i].UpdatePos();
+             balls[i].Draw();
+        }
+    }
+}
